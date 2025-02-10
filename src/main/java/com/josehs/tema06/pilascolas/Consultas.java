76bb3e9e652784380ea505d9ca5ab6f3837a0136
JoseHS2002 <joherso16@gmail.com>
2025-02-10 15:15:27 +0100

    Ejercicio6.java

---
+++ src/main/java/com/josehs/tema06/pilascolas/Consultas.java
@@ -0,0 +1,14 @@
+package com.josehs.tema06.pilascolas;
+
+public class Consultas {
+    public static void main(String[] args) {
+        System.out.println("********************");
+        System.out.println("** C O N S U L T A S **");
+        System.out.println("********************");
+        System.out.println("1. Citas de un paciente");
+        System.out.println("2. Citas de un médico");
+        System.out.println("3. Mostrar todos los pacientes");
+        System.out.println("4. Mostrar todas las citas");
+    }
+
+}
